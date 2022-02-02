a5a655e587ef28421030e9e6e3b918e961e704d1
Austin Flatt <austindflatt@gmail.com>
2022-02-02 14:38:54 -0500

    parse first and last name

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ async function asyncAxios() {
     console.log('asyncResponse');
     let asyncResponse = await axios.get(URL);
     console.log(asyncResponse.data);
+    // Get first and last name
+    let firstName = asyncResponse.data.results[0].name.first;
+    let lastName = asyncResponse.data.results[0].name.last;
+    let fullName = `${firstName} ${lastName}`;
+    console.log(fullName);
 }
 
 asyncAxios();
